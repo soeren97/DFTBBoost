b4bb6471ce91914aef5b83d3ceb11065f18fafdc
S0ren Langkilde <soerenl@fend01.cluster>
2023-01-22 14:03:25 +0100

    Modified analyze dataset to compute distance between matrices

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -16,26 +16,40 @@ def compare_datasets(CONFIG):
     files = os.listdir(path)
     delta_HOMO_LUMO = []
     delta_all = []
+    delta_Matrix = []
     for i in tqdm(files, desc="Calc dft, dftb diff", leave=False):
         data = pd.read_pickle(path + i)
 
+        dftb = data["X"]
+        Y = data["Y"]
         n_electrons = data["N_electrons"].to_list()
-        dftb = data["NN_X"]
-        dft = data["NN_Y"]
 
-        dftb_eigenvalues, dftb_HOMO, dftb_LUMO = find_eigenvalues(dftb, n_electrons)
-        dft_eigenvalues, dft_HOMO, dft_LUMO = find_eigenvalues(dft, n_electrons)
+        n_orbitals = [row[3] for row in Y]
+        dft = [row[4] for row in Y]
+
+        dftb_eigenvalues, dftb_HOMO, dftb_LUMO = find_eigenvalues(
+            dftb, n_electrons, n_orbitals
+        )
+        dft_eigenvalues, dft_HOMO, dft_LUMO = find_eigenvalues(
+            dft, n_electrons, n_orbitals
+        )
+        distance = torch.dist(dftb, dft)
 
         delta_all.append(np.average(abs(dftb_eigenvalues - dft_eigenvalues)))
+
         delta_HOMO_LUMO.append(
             np.average(abs(dftb_HOMO - dft_HOMO) + abs(dftb_LUMO - dft_LUMO))
         )
 
+        delta_Matrix.append(distance)
+
     mean_delta_all = np.mean(delta_all)
     mean_delta_HOMO_LUMO = np.mean(delta_HOMO_LUMO)
+    mean_distance = np.mean(delta_Matrix)
 
-    CONFIG["dftb_dft_delta_all"] = float(mean_delta_all)
+    CONFIG["dftb_dft_delta_All"] = float(mean_delta_all)
     CONFIG["dftb_dft_delta_HOMO_LUMO"] = float(mean_delta_HOMO_LUMO)
+    CONFIG["dftb_dft_delta_All"] = float(mean_distance)
 
     with open(r"model_config/config.yaml", "w") as config_file:
         updated_file = yaml.dump(CONFIG, config_file)
