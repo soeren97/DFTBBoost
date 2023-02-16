998758cafa29eef93c458807338f2dcbe5d30ec2
S0ren Langkilde <soerenl@fend01.cluster>
2023-02-16 12:17:59 +0100

    Added calculation of std in mean error dft dftb

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -47,67 +47,17 @@ def compare_datasets(CONFIG):
     mean_delta_HOMO_LUMO = np.mean(delta_HOMO_LUMO)
     mean_distance = np.mean(delta_Matrix)
 
+    std_delta_all = np.std(delta_all)
+    std_delta_HOMO_LUMO = np.std(delta_HOMO_LUMO)
+    std_distance = np.std(delta_Matrix)
+
     CONFIG["dftb_dft_delta_All"] = float(mean_delta_all)
     CONFIG["dftb_dft_delta_HOMO_LUMO"] = float(mean_delta_HOMO_LUMO)
     CONFIG["dftb_dft_delta_Matrix"] = float(mean_distance)
 
-    with open(r"model_config/config.yaml", "w") as config_file:
-        updated_file = yaml.dump(CONFIG, config_file)
-
-
-def compare_datasets2(CONFIG):
-    path = "Data/datasets/NN/"
-    files = os.scandir(path)
-
-    dftb_list = []
-    dft_list = []
-    n_electrons_list = []
-    n_orbitals_list = []
-
-    for file in files:
-        data = pd.read_pickle(file.path)
-        dftb_list.append(data["X"])
-        dft_list = dft_list.append([row[4] for row in data["Y"]])
-        n_electrons_list.append(data["N_electrons"])
-        n_orbitals_list.append([row[3] for row in data["Y"]])
-
-    dftb = torch.stack(dftb_list)
-    dft = torch.stack(dft_list)
-    n_electrons = np.concatenate(n_electrons_list)
-    n_orbitals = np.concatenate(n_orbitals_list)
-
-
-def compare_datasets3(CONFIG):
-    path = f"Data/datasets/NN/"
-    files = os.listdir(path)
-
-    # Load data from all files at once
-    data = np.concatenate([np.load(path + i, allow_pickle=True) for i in files])
-
-    # Extract relevant data from the arrays
-    dftb = data["X"]
-    Y = data["Y"]
-    n_electrons = data["N_electrons"]
-    n_orbitals = Y[:, 3]
-    dft = Y[:, 4]
-
-    # Perform calculations on the arrays using numpy functions
-    dftb_eigenvalues, dftb_HOMO, dftb_LUMO = find_eigenvalues(
-        dftb, n_electrons, n_orbitals
-    )
-    dft_eigenvalues, dft_HOMO, dft_LUMO = find_eigenvalues(dft, n_electrons, n_orbitals)
-    distance = np.linalg.norm(dft - dftb, axis=1)
-
-    # Compute mean of the arrays
-    mean_delta_all = np.mean(abs(dftb_eigenvalues - dft_eigenvalues))
-    mean_delta_HOMO_LUMO = np.mean(
-        abs(dftb_HOMO - dft_HOMO) + abs(dftb_LUMO - dft_LUMO)
-    )
-    mean_distance = np.mean(distance)
-
-    CONFIG["dftb_dft_delta_All"] = float(mean_delta_all)
-    CONFIG["dftb_dft_delta_HOMO_LUMO"] = float(mean_delta_HOMO_LUMO)
-    CONFIG["dftb_dft_delta_All"] = float(mean_distance)
+    CONFIG["dftb_dft_std_All"] = float(std_delta_all)
+    CONFIG["dftb_dft_std_HOMO_LUMO"] = float(std_delta_HOMO_LUMO)
+    CONFIG["dftb_dft_std_Matrix"] = float(std_distance)
 
     with open(r"model_config/config.yaml", "w") as config_file:
         updated_file = yaml.dump(CONFIG, config_file)
