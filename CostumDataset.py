00934acb6ecc9f214bacf859025c08cee7c0b3d1
S0ren Langkilde <soerenl@fend01.cluster>
2023-04-15 01:51:49 +0200

    Adjustments to enable training of NN models

--- CostumDataset.py
+++ CostumDataset.py
@@ -18,18 +18,42 @@ class CostumDataset(Dataset):
         return len(self.file_names)
 
     def __getitem__(self, index: int) -> List[Any]:
-        data = pd.read_pickle(self.file_names[index])
+        if self.ml_method != "NN":
+            data = pd.read_pickle(self.file_names[index])
 
-        X = data["X"].tolist()
+            X = data["X"].tolist()
 
-        Y = data["Y"]
+            Y = data["Y"]
 
-        eigenvalues = [row[0] for row in Y]
+            eigenvalues = [row[0] for row in Y]
 
-        N_orbitals = [row[1] for row in Y]
+            N_orbitals = [row[1] for row in Y]
 
-        ham_over = [row[2] for row in Y]
+            ham_over = [row[2] for row in Y]
 
-        N_electrons = data["N_electrons"].tolist()
+            N_electrons = data["N_electrons"].tolist()
 
-        return [X, eigenvalues, ham_over, N_electrons, N_orbitals]
+            return [X, eigenvalues, ham_over, N_electrons, N_orbitals]
+
+        else:
+            data = pd.read_pickle(self.file_names[index])
+
+            X = torch.stack(data["X"].tolist())
+
+            Y = data["Y"]
+
+            eigenvalues = torch.stack([row[0] for row in Y])
+
+            N_orbitals = torch.stack([torch.tensor(row[1]) for row in Y])
+
+            ham_over = torch.stack([row[2] for row in Y])
+
+            N_electrons = torch.stack(data["N_electrons"].apply(torch.tensor).tolist())
+
+            return {
+                "X": X,
+                "eigenvalues": eigenvalues,
+                "ham_over": ham_over,
+                "N_electrons": N_electrons,
+                "N_orbitals": N_orbitals,
+            }
