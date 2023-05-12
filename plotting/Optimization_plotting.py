767d51ff8ad471b57d4236b19aabdecec92696cc
S0ren Langkilde <soerenl@fend01.cluster>
2023-05-12 23:12:21 +0200

    cleanup

--- plotting/Optimization_plotting.py
+++ plotting/Optimization_plotting.py
@@ -1,6 +1,9 @@
 import os
 from typing import Optional, Tuple
 
+import matplotlib.pyplot as plt
+from plotly.graph_objects import Figure
+
 import optuna
 from optuna.visualization import (
     plot_param_importances,
@@ -12,32 +15,52 @@ from optuna.visualization import (
 def load_study(path: Optional[str] = None) -> Tuple[optuna.study.Study, str]:
     root_dir = "Optuna/"
     if path == None:
-        studies = os.listdir("root_dir")
+        studies = os.listdir(root_dir)
 
         studies = sorted(
-            studies, key=lambda x: os.path.getmtime(os.path.join("Optuna", x))
+            studies, key=lambda x: os.path.getmtime(os.path.join(root_dir, x))
         )
-        folder = os.path.join(root_dir, studies[-1])
+        folder = os.path.join(root_dir, studies[-1], "study.db")
     else:
-        folder = os.path.join(root_dir, path)
+        folder = os.path.join(root_dir, path, "study.db")
 
-    study = optuna.load_study(storage=f"{folder}study.db")
+    storage = f"sqlite:///{folder}"
 
-    return study, folder
+    study = optuna.load_study(storage=storage, study_name="GNN")
+
+    return study, folder[:-8]
+
+
+def save_image(
+    path: str, plotly_figure: Figure, update_traces: Optional[bool] = False
+) -> None:
+    plotly_figure.update_layout(plot_bgcolor="white")
+
+    if update_traces:
+        plotly_figure.update_traces(texttemplate=None, textposition="outside", text="")
+
+    fig.update_layout(title="")
+
+    fig.update_layout(autosize=True)
+
+    plotly_figure.write_image(os.path.join(save_folder, path))
 
 
 if __name__ == "__main__":
     # Load study
-    completed_study, save_folder = load_study()
+    completed_study, save_folder = load_study("23_04_13_15_25")
 
     # Plot importance
     fig = plot_param_importances(completed_study)
-    fig.write_image(os.path.join(save_folder, "param_imporatnce.png"))
+
+    save_image("param_imporatnce.png", fig, update_traces=True)
 
     # Plot history
     fig = plot_optimization_history(completed_study)
-    fig.write_image(os.path.join(save_folder, "optimization_history.png"))
+
+    save_image("optimization_history.png", fig)
 
     # Plot param dependencies
     fig = plot_parallel_coordinate(study=completed_study)
-    fig.write_image(os.path.join(save_folder, "param_dependencies.png"))
+
+    save_image("param_dependencies.png", fig)
