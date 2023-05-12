1699f7c3c39803fee47bc1f7207f48877161a1b9
S0ren Langkilde <soerenl@fend01.cluster>
2023-05-12 23:12:02 +0200

    Remove unused code

--- plotting/training_plotting.py
+++ plotting/training_plotting.py
@@ -1,33 +1,17 @@
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import swifter
 import numpy as np
 import torch
 
 from numpy.typing import NDArray
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 
 import sys
 
 sys.path.append(os.getcwd())
 
 from utils import load_config, extract_fock, extract_overlap, freedman_diaconis_bins
-
-sns.set_style("white")
-sns.set_style("ticks")
-
-
-swifter.set_defaults(
-    npartitions=8,
-    dask_threshold=2,
-    scheduler="processes",
-    progress_bar=False,
-    progress_bar_desc=None,
-    allow_dask_on_strings=False,
-    force_parallel=False,
-)
 
 
 class Plotter:
@@ -109,31 +93,31 @@ class Plotter:
             label="Average dft-dftb error",
         )
 
-        ax.axhspan(
-            ymin=self.goal_line - self.goal_error,
-            ymax=self.goal_line + self.goal_error,
-            facecolor=".5",
-            alpha=0.3,
-            label="dft-dftb STD",
-        )
+        # ax.axhspan(
+        #     ymin=self.goal_line - self.goal_error,
+        #     ymax=self.goal_line + self.goal_error,
+        #     facecolor=".5",
+        #     alpha=0.3,
+        #     label="dft-dftb STD",
+        # )
 
-        zoomed_ax = ax.inset_axes([0.5, 0.3, 0.4, 0.45])
+        zoomed_ax = ax.inset_axes([0.62, 0.52, 0.35, 0.45])
         zoomed_ax.plot(loss_train[-15:], color="dodgerblue")
         zoomed_ax.plot(loss_test[-15:], color="darkorange")
         zoomed_ax.axhline(
             y=self.goal_line, linestyle="dotted", alpha=0.5, color="black"
         )
 
-        zoomed_ax.axvspan(
-            ymin=self.goal_line - self.goal_error,
-            ymax=self.goal_line + self.goal_error,
-            xmin=len(loss_test) - 15,
-            xmax=len(loss_test) - 1,
-            facecolor=".5",
-            alpha=0.3,
-        )
+        # zoomed_ax.axvspan(
+        #     ymin=self.goal_line - self.goal_error,
+        #     ymax=self.goal_line + self.goal_error,
+        #     xmin=len(loss_test) - 15,
+        #     xmax=len(loss_test) - 1,
+        #     facecolor=".5",
+        #     alpha=0.3,
+        # )
 
-        zoomed_ax.set_ylim(bottom=0, top=3)
+        zoomed_ax.set_ylim(bottom=0, top=2.5)
 
         ax.indicate_inset_zoom(zoomed_ax, edgecolor="grey")
 
@@ -149,7 +133,11 @@ class Plotter:
         plt.close()
 
     def plot_energies(
-        self, energies_true: pd.Series, energies_pred: pd.Series, data_name: str
+        self,
+        energies_true: pd.Series,
+        energies_pred: pd.Series,
+        data_name: str,
+        split: bool,
     ) -> None:
         fig, ax = plt.subplots()
 
@@ -157,72 +145,150 @@ class Plotter:
         energies_true = np.hstack(energies_true[0])
         energies_pred = np.hstack(energies_pred[0])
 
+        # Convert to eV
+        # energies_true *= 27.2114
+        # energies_pred *= 27.2114
+
+        if split:
+            # remove core orbitals
+            mask = energies_true > -5
+
+            energies_true = energies_true[mask]
+            energies_pred = energies_pred[mask]
+            name = "valence"
+
+        else:
+            name = "full"
+
         # Remove padding
         stacked_true = energies_true[energies_true != 0]
         stacked_pred = energies_pred[energies_pred != 0]
 
-        # Normalize
-        max_energy = max(stacked_true.max(), stacked_pred.max())
-        min_energy = min(stacked_true.min(), stacked_pred.min())
+        # Compute density matrix
+        _, xedges, yedges = np.histogram2d(stacked_pred, stacked_true, bins=200)
 
-        norm_true = (stacked_true + abs(min_energy)) / (max_energy + abs(min_energy))
-        norm_pred = (stacked_pred + abs(min_energy)) / (max_energy + abs(min_energy))
+        im = ax.hist2d(stacked_pred, stacked_true, bins=200, cmin=1)
 
-        ax.scatter(
-            norm_true,
-            norm_pred,
-            linewidths=1,
-            label="Energy correlation",
-        )
+        # Add colorbar
+        cb = fig.colorbar(im[3], ax=ax)
+        cb.set_label("Count")
+        save_name = f"heatmap_{data_name}_{name}.png"
+
         ax.plot(
-            [0, 1],
-            [0, 1],
+            [xedges[0], xedges[-1]],
+            [xedges[0], xedges[-1]],
             label="Perfect correlation",
             color="grey",
             linestyle="dashed",
         )
 
-        ax.set_xlim(left=0, right=1)
-        ax.set_ylim(bottom=0, top=1)
+        ax.set_xlim(left=xedges[0], right=xedges[-1])
+        ax.set_ylim(bottom=xedges[0], top=xedges[-1])
 
-        plt.xlabel("Normalized eigen energy predictions [Ha]")
-        plt.ylabel("Normalized true eigen energies [Ha]")
+        plt.xlabel("Eigen energy predictions")
+        plt.ylabel("True eigen energies")
         plt.tight_layout()
         plt.legend()
-        plt.savefig(self.save_dir + f"energies_{data_name}.png", dpi=600)
+        plt.savefig(self.save_dir + save_name, dpi=600)
         plt.close()
 
     def plot_distribution(
-        self, energies_true: pd.Series, energies_pred: pd.Series, data_name: str
+        self,
+        energies_true: pd.Series,
+        energies_pred: pd.Series,
+        data_name: str,
+        split: bool,
     ) -> None:
         # Stack energies of all molecules
         energies_true = np.hstack(energies_true[0])
         energies_pred = np.hstack(energies_pred[0])
 
-        # Remove padding
-        stacked_true = energies_true[energies_true != 0]
-        stacked_pred = energies_pred[energies_pred != 0]
+        if split:
+            # devide into  orbital energies
+            mask_high = -21 >= energies_true
+            mask1 = mask_high
 
-        bin_size = 0.8  # Set the bin size
-        bin_range = (
-            np.min([stacked_pred, stacked_true]),
-            np.max([stacked_pred, stacked_true]),
-        )  # Set the bin range
-        bins = np.arange(*bin_range, bin_size)  # Create an array of bin edges
+            mask_high = -16 >= energies_true
+            mask_low = energies_true > -21
+            mask2 = np.logical_and(mask_high, mask_low)
 
-        plt.hist(
-            [stacked_pred, stacked_true],
-            label=["Predicted eigenenergies", "True eigenenergies"],
-            bins=bins,
-        )
-        plt.xlabel("Eigenenergy [Ha]")
-        plt.ylabel("Frequency")
-        plt.yscale("log")
+            mask_high = -12 >= energies_true
+            mask_low = energies_true > -16
+            mask3 = np.logical_and(mask_high, mask_low)
 
-        plt.legend()
-        plt.tight_layout()
-        plt.savefig(self.save_dir + f"energy_distribution_{data_name}.png", dpi=600)
-        plt.close()
+            mask_high = -5 >= energies_true
+            mask_low = energies_true > -12
+            mask4 = np.logical_and(mask_high, mask_low)
+
+            mask_low = energies_true > -5
+            mask5 = mask_low
+
+            # energies_true = energies_true[mask]
+
+            # energies_pred = energies_pred[mask]
+
+            energy_list_true = [
+                energies_true[mask1],
+                energies_true[mask2],
+                energies_true[mask3],
+                energies_true[mask4],
+                energies_true[mask5],
+            ]
+            energy_list_pred = [
+                energies_pred[mask1],
+                energies_pred[mask2],
+                energies_pred[mask3],
+                energies_pred[mask4],
+                energies_pred[mask5],
+            ]
+            sections = [
+                "core_lowest",
+                "core_low",
+                "core_high",
+                "core_highest",
+                "valence",
+            ]
+
+        else:
+            energy_list_true = [energies_true]
+            energy_list_pred = [energies_pred]
+            sections = ["full"]
+
+        for energies_pred, energies_true, section in zip(
+            energy_list_pred, energy_list_true, sections
+        ):
+            # Remove padding
+            stacked_true = energies_true[energies_true != 0]
+            stacked_pred = energies_pred[energies_pred != 0]
+
+            # bin_size = 0.8  # Set the bin size
+            # bin_range = (
+            #     np.min([stacked_pred, stacked_true]) - 2,
+            #     np.max([stacked_pred, stacked_true]) + 2,
+            # )  # Set the bin range
+            # np.arange(*bin_range, bin_size)  # Create an array of bin edges
+
+            try:
+                bins = freedman_diaconis_bins(stacked_pred)
+            except IndexError:
+                continue
+
+            plt.hist(
+                [stacked_pred, stacked_true],
+                label=["Predicted eigenenergies", "True eigenenergies"],
+                bins=bins,
+            )
+            plt.xlabel("Eigenenergy [Ha]")
+            plt.ylabel("Frequency")
+            # plt.yscale("log")
+
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(
+                self.save_dir + f"energy_distribution_{data_name}_{section}.png",
+                dpi=600,
+            )
+            plt.close()
 
     def main(self, path: str = None) -> None:
         self.path = path
@@ -249,14 +315,24 @@ class Plotter:
 
         self.plot_loss(loss)
 
-        self.plot_energies(energy_preds[1], energy_true[1], "test")
+        # Plot correlation
+        self.plot_energies(energy_true[1], energy_preds[1], "test", split=False)
 
-        self.plot_energies(energy_preds[2], energy_true[2], "valid")
+        self.plot_energies(energy_true[2], energy_preds[2], "valid", split=False)
 
-        self.plot_distribution(energy_preds[1], energy_true[1], "test")
+        self.plot_energies(energy_true[1], energy_preds[1], "test", split=True)
 
-        self.plot_distribution(energy_preds[2], energy_true[2], "valid")
+        self.plot_energies(energy_true[2], energy_preds[2], "valid", split=True)
+
+        # Plot histogram of energies
+        self.plot_distribution(energy_true[1], energy_preds[1], "test", split=True)
+
+        self.plot_distribution(energy_true[1], energy_preds[1], "test", split=False)
+
+        self.plot_distribution(energy_true[2], energy_preds[2], "valid", split=True)
+
+        self.plot_distribution(energy_true[2], energy_preds[2], "valid", split=False)
 
 
 if __name__ == "__main__":
-    Plotter().main()
+    Plotter().main("1681562523")
