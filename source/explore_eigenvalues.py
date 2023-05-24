3a34427664aca136176efe07f37b58eca33b6a7d
soeren97 <soeren@langkilde.com>
2023-05-24 21:16:01 +0200

    corrections to pass hook

--- source/explore_eigenvalues.py
+++ source/explore_eigenvalues.py
@@ -1,15 +1,30 @@
-import re
+"""Explore eigenenergies to confirm distribution is calculated correctly. """
+
 import os
-from tqdm import tqdm
-import numpy as np
-from source.utils import freedman_diaconis_bins
-import matplotlib.pyplot as plt
 import pickle
+import re
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import NDArray
+from tqdm import tqdm
 
 
-def load_eigenvalues(path):
+def load_eigenvalues(path: str) -> NDArray:
+    """Load Eigenvalues from folder of G16 calculations.
+
+    Args:
+        path (str): Path to folder
+
+    Returns:
+        NDArray: Eigenenergies.
+    """
     eigenvalue_list = []
     generator = os.walk(path)
+    regex_string = (
+        r"(?<=Population analysis using the SCF density"
+        + r"\.\n\n \*{70}\n)\n([\S\s]*)Condensed to atoms"
+    )
 
     # skip first iteration of the generator
     next(generator)
@@ -24,11 +39,11 @@ def load_eigenvalues(path):
         with open(log_path, "r") as log_file:
             log_text = log_file.read()
             eigenvalues_pattern = re.compile(
-                r"(?<=Population analysis using the SCF density\.\n\n \*{70}\n)\n([\S\s]*)Condensed to atoms",
+                regex_string,
             )
-            eigenvalues_match = eigenvalues_pattern.search(log_text)
-            if eigenvalues_match:
-                eigenvalues_lines = eigenvalues_match.group(1).strip().split("\n")
+            match = eigenvalues_pattern.search(log_text)
+            if match:
+                eigenvalues_lines = match.group(1).strip().split("\n")
                 eigenvalues = []
                 for line in eigenvalues_lines:
                     line = re.sub(r"[^0-9\s.-]", "", line)
